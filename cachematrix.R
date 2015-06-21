5586c9c989555c66d6d719a0500c14a4343583f0
David Pérez <tornilleiro@gmail.com>
2015-06-21 17:06:51 +0200

    Final version aome extra checks like, square matrix or determinant!=0

--- cachematrix.R
+++ cachematrix.R
@@ -2,6 +2,8 @@
 ## functions do
 
 ## Write a short comment describing this function
+#  getter an setter functions to save data values of matrix
+
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -19,7 +21,13 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
+
 ## Write a short comment describing this function
+#  1. if the X object has inverse. I return the value.
+#  2. If the X object x has not inverse calculated, I check following cases
+#     2.1 I check if the matrix is square.
+#     2.2 I check if the determinant of matrix is not 0.
+#  3. If two steps (2.1 and 2.1) before are ok I use the funcion solve  to calculate the inverse
 
 cacheSolve <- function(x) {
     
@@ -29,7 +37,19 @@ cacheSolve <- function(x) {
         message("getting inverse matrix cached data")
         return(m)
     }
+
     data <- x$get()
+    di<-dim(data)
+    if(di[1]!=di[2]) {
+        message("The matrix is not square") 
+        return(-1)
+    }
+    d<-det(data)
+    if (d==0){
+      message("This is a singular matrix, can not be inverted")
+      return(-1)
+    }
+    
      m <- solve(data)
     x$setinverse(m)
     m
